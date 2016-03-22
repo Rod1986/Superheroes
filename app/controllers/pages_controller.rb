e3e07715568b4bcb767a5823550cabf7555b692f
Rodrigo <hein.rodrigo@gmail.com>
2016-03-22 19:57:49 -0300

    voting database created and working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,18 @@ class PagesController < ApplicationController
 
   def batman_vs_superman
   end
+
+  def save_vote
+    if params[:heroe].present? && params[:email].present?
+      Vote.new(
+        heroe: params[:heroe],
+        email: params[:email]
+        ).save
+      redirect_to pages_batman_vs_superman_path, notice: 'Vote submited'
+     else
+      flash[:alert] = 'Please fill the required information'
+      render :index
+     end
+  end
+
 end
